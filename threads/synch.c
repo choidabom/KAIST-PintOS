b7d7cfceef0ae75a664f19d7dc68639dc144e84b
Ubuntu <ubuntu@ip-172-31-31-40.ec2.internal>
2022-11-16 19:49:50 +0000

    kaist_pitos week01

--- threads/synch.c
+++ threads/synch.c
@@ -69,7 +69,6 @@ void sema_down(struct semaphore *sema)
 	while (sema->value == 0)
 	// 현재 cpu(공유자원)를 차지하고 있어요
 	{
-		// list_push_back(&sema->waiters, &thread_current()->elem);
 		list_insert_ordered(&sema->waiters, &thread_current()->elem, cmp_priority, NULL);
 		thread_block();
 	}
@@ -115,6 +114,8 @@ void sema_up(struct semaphore *sema)
 	old_level = intr_disable();
 	if (!list_empty(&sema->waiters))
 	{
+		// list_sort(&sema->waiters, cmp_sem_priority, NULL);
+		list_sort(&sema->waiters, cmp_priority, NULL);
 		thread_unblock(list_entry(list_pop_front(&sema->waiters), struct thread, elem));
 	}
 	sema->value++;
@@ -195,11 +196,11 @@ void lock_acquire(struct lock *lock)
 	ASSERT(!intr_context());
 	ASSERT(!lock_held_by_current_thread(lock));
 
-	if (!(lock->holder == NULL))
+	if (lock->holder != NULL)
 	{
 		thread_current()->wait_on_lock = lock;
 		// 획득하고자 하는 lock의 주소를 저장.
-		list_insert_ordered(&lock->holder->donations, &thread_current()->donation_elem, cmp_dona_priority, NULL);
+		list_insert_ordered(&lock->holder->donations, &thread_current()->donation_elem, cmp_priority, NULL);
 		donate_priority();
 	}
 	sema_down(&lock->semaphore);
--- threads/thread.c
+++ threads/thread.c
@@ -323,14 +323,9 @@ void thread_yield(void)
 /* Sets the current thread's priority to NEW_PRIORITY. */
 void thread_set_priority(int new_priority)
 {
-	thread_current()->priority = new_priority;
-	// if (!list_empty(&thread_current()->donations))
-	// {
-	// 	struct thread *max_t = list_entry(list_begin(&thread_current()->donations), struct thread, donation_elem);
-	// 	if (thread_current()->priority < max_t->priority)
-	// 		thread_current()->priority = max_t->priority;
-	// }
-	// donate_priority();
+	thread_current()->init_priority = new_priority;
+	refresh_priority();
+	donate_priority();
 	test_max_priority();
 }
 
@@ -706,20 +701,29 @@ bool cmp_dona_priority(const struct list_elem *a_,
 void donate_priority(void)
 {
 	struct thread *a = thread_current();
-	struct thread *b = thread_current()->wait_on_lock->holder;
+	struct thread *b;
 	int depth = 0;
 	while (depth < 8)
 	{
+		if (a->wait_on_lock == NULL)
+			break;
+		b = a->wait_on_lock->holder;
 		if (&a->priority > &b->priority)
 			b->priority = a->priority;
 		else
 			break;
-		a = a->wait_on_lock->holder;
-		b = b->wait_on_lock->holder;
-		if (!is_thread(b))
-			break;
+		a = b;
 		depth++;
 	}
+	// struct thread *cur = thread_current();
+	// for (int i = 0; i < 8; i++)
+	// {
+	// 	if (cur->wait_on_lock == NULL)
+	// 		break;
+	// 	struct thread *hold = cur->wait_on_lock->holder;
+	// 	hold->priority = cur->priority;
+	// 	cur = hold;
+	// }
 	/* priority donation을 수행하는 함수를 구현한다.
 현재 스레드가 기다리고 있는 lock 과 연결된 모든 스레드들을 순회하며
 현재 스레드의 우선순위를 lock을 보유하고 있는 스레드에게 기부한다.
@@ -729,26 +733,19 @@ void donate_priority(void)
 
 void remove_with_lock(struct lock *lock)
 {
-	struct list donation_list = lock->holder->donations;
-	if (!list_empty(&donation_list))
+	if (!list_empty(&lock->holder->donations))
 	{
 		struct list_elem *s;
-		struct thread *t;
 
-		s = list_begin(&donation_list);
-		while (s != list_end(&donation_list))
+		s = list_begin(&lock->holder->donations);
+		while (s != list_end(&lock->holder->donations))
 		{
-			// t = list_entry(s, struct thread, donation_elem);
-			// if (!is_thread(t))
-			// {
-			// 	return;
-			// }
-
-			// if (t->wait_on_lock == lock)
-			// {
-			// 	s = list_remove(s);
-			// 	continue;
-			// }
+			struct thread *t = list_entry(s, struct thread, donation_elem);
+			if (t->wait_on_lock == lock)
+			{
+				s = list_remove(s);
+				continue;
+			}
 			s = list_next(s);
 		}
 	}
